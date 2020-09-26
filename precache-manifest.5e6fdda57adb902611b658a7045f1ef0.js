bfc8c068b7940f759abbd6da1a7fc452474c8e4f
Sambhav Jain <sambhav@xencov.com>
2020-09-27 01:40:56 +0530

    Updates

---
+++ precache-manifest.5e6fdda57adb902611b658a7045f1ef0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7a71ecef56b085ca1663b1f974863d9",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "e8cb8889d4e840574435",
+    "url": "/todo/static/css/2.98828b2b.chunk.css"
+  },
+  {
+    "revision": "43e226081537244c6d53",
+    "url": "/todo/static/css/main.e50cae06.chunk.css"
+  },
+  {
+    "revision": "e8cb8889d4e840574435",
+    "url": "/todo/static/js/2.2ad2027d.chunk.js"
+  },
+  {
+    "revision": "05f004cf1ac1b697d59f67cd14a990d6",
+    "url": "/todo/static/js/2.2ad2027d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43e226081537244c6d53",
+    "url": "/todo/static/js/main.542584fb.chunk.js"
+  },
+  {
+    "revision": "1a4e95f513371874a3ba",
+    "url": "/todo/static/js/runtime-main.f5249c09.js"
+  }
+]);
